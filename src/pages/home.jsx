7f345d5f35e3b5ab8cff2962e6311b252d85fdb3
GParej0 <195317371+GParej0@users.noreply.github.com>
2026-07-02 12:19:41 +0200

    adjust layouts and add new assets

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,8 @@
 import {Link} from "react-router"
 import Diamond from "../assets/diamond-svgrepo-com.svg"
+import Certificate from "../assets/certificate-badge-svgrepo-com.svg"
+import Gift from "../assets/gift-svgrepo-com.svg"
+import Shipping from "../assets/shipping-truck-svgrepo-com.svg"
 export default function Home (){
     return (
     <div className="home-container">
@@ -7,25 +10,31 @@ export default function Home (){
         <img src={Diamond} alt="Diamond" />
         <h1>Jewelry made to last</h1>
         <h2>Timeless pieces sourced from FakeStore's jewelery collection</h2>
-        <Link to="products">Shop the collection</Link>
+        <Link to="products" className="cta-button">Browse the collection</Link>
       </div>
       <div className="home-info">
         <h2>Why luxe.</h2>
         <ul>
           <li>
-            <img src="" alt=""/>
+            <img src={Certificate} alt="Certificate"/>
+            <div>
             <h3>Certified pieces</h3>
             <p>Quality you can trust</p>
+            </div>
           </li>
           <li>
-            <img src="" alt=""/>
+            <img src={Gift} alt="Gift"/>
+            <div>
             <h3>Gift ready</h3>
             <p>Packaged with care</p>
+            </div>
           </li>
           <li>
-            <img src="" alt=""/>
+            <img src={Shipping} alt="Shipping"/>
+            <div>
             <h3>Free shipping</h3>
             <p>On every order</p>
+            </div>
           </li>
         </ul>
       </div>
